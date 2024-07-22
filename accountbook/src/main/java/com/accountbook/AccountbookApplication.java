68544645c584df08599cef61ca9cf853a8cd3b87
user <user@DESKTOP-Q72AEDN>
2024-07-22 20:52:43 +0900

    스프링 시큐리티 로그인 창 나오지 않도록 설정

--- accountbook/src/main/java/com/accountbook/AccountbookApplication.java
+++ accountbook/src/main/java/com/accountbook/AccountbookApplication.java
@@ -2,8 +2,9 @@ package com.accountbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AccountbookApplication {
 
 	public static void main(String[] args) {
