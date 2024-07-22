828baf4c76244595e2ef52a0c5cceffa986b0583
user <user@DESKTOP-Q72AEDN>
2024-07-22 20:53:25 +0900

    findByMbId 추가

--- accountbook/src/main/java/com/accountbook/repository/MemberRepository.java
+++ accountbook/src/main/java/com/accountbook/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accountbook.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
 
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+	Member findByMbId(String mbId);
 }
