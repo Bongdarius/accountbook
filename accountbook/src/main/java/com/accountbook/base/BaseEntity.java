9ee4f5827e93dd9cd133eeeddc88a1bfc67d94c3
user <user@DESKTOP-Q72AEDN>
2024-07-22 18:32:59 +0900

    Getter, Setter, ToString 추가

--- accountbook/src/main/java/com/accountbook/base/BaseEntity.java
+++ accountbook/src/main/java/com/accountbook/base/BaseEntity.java
@@ -4,8 +4,14 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @MappedSuperclass
+@Getter
+@Setter
+@ToString
 public class BaseEntity {
 	
 	@Column(nullable = true)
