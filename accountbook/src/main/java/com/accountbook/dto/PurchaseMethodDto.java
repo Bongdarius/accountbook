f353566e3dea43b36a861db628dee97898a65a2d
Bongdarius <okuo94@gnu.ac.kr>
2024-08-07 00:50:18 +0900

    결제수단 신규 개발

---
+++ accountbook/src/main/java/com/accountbook/dto/PurchaseMethodDto.java
@@ -0,0 +1,13 @@
+package com.accountbook.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PurchaseMethodDto {
+    private Integer pcmSeq;
+
+    private String pcmNm;
+
+}
---
+++ accountbook/src/main/java/com/accountbook/service/PurchaseMethodService.java
@@ -0,0 +1,14 @@
+package com.accountbook.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.accountbook.base.BaseService;
+import com.accountbook.entity.Card;
+import com.accountbook.entity.PurchaseMethod;
+
+public interface PurchaseMethodService extends BaseService<PurchaseMethod>{
+
+	public List<PurchaseMethod> initDatas() throws Exception;
+}
