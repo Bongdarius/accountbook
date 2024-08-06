25807ba9cc2ef660aae84e5994ab9cddf45687ef
Bongdarius <okuo94@gnu.ac.kr>
2024-08-07 00:49:16 +0900

    List<Card> findByOrderByCardSeqAsc();
    추

--- accountbook/src/main/java/com/accountbook/repository/CardRepository.java
+++ accountbook/src/main/java/com/accountbook/repository/CardRepository.java
@@ -1,9 +1,11 @@
 package com.accountbook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accountbook.entity.Card;
 
 public interface CardRepository extends JpaRepository<Card, Integer> {
-
+	List<Card> findByOrderByCardSeqAsc();
 }
